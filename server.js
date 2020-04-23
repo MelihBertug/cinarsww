122a1d6558bced59c2f9487fbfa42b4f0c184851
Glitch (hello-express) <none>
2020-04-23 17:07:17 +0000

    🏞📖 Checkpoint ./server.js:15069167/6877

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
 var mineflayer = require('mineflayer');
-
-var db = require('quick.db')
-
+var pass = "12345"; //Authme şifresini girin
 var ayar = {
-    host: "crayzcrafthubb.aternos.me", //Buraya sunucu IPsini girin.
-    port: 25565,                      //Buraya sunucu portunuzu girin. Genellikle 25565 olarak ayarlıdır.
-    username: "ADMIN",               //Buraya botunuzun ismini girin.
-    version: false                  //Burayı değiştirmenize gerek yok.
+    host: "play.creativeorsurvival.net", //Ip
+    port: 25565, //Değiştirmeyin
+    username: "BotName", //Botun ismi
+   version: false //Değiştirmeyin
 };
-
-var authme = {
-  authme: 'mevcut', //Buraya mevcut değilse değil yazınız.
-  sifre: 'AUTHME_SIFRESI'
-}
 
 var bot = mineflayer.createBot(ayar);
 
 
 bot.on('chat', function(username, message) {
   if (username === bot.username) return;
-  
   function intervalFunc() {
     bot.setControlState('forward', true)
      }
-  
     setInterval(intervalFunc,7000);
-  
-  let giris = require('giris') //Burayı kesinlikle değiştirmeyin.
-  if (!giris) {
-    bot.chat(`/register ${authme.sifre} ${authme.sifre}`)
-    console.log('Bot sunucuya başarıyla kayıt oldu!')
-    db.set('giris', 'tamamlandi')
-  }
-  if (giris == 'tamamlnadi') {
-    bot.chat(`/login ${authme.sifre}`)
-    console.log('Bot sunucuya giriş yaptı!')
-  }
+  console.log('Sunucuya giriş yapıldı!');
+  bot.chat('/login '+ pass);
 });
 
 bindEvents(bot);
